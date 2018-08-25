8075c2c14a41731cc24baec66c4cb5bbff4f2932
Ward <Ward_Stapper@hotmail.com>
2018-08-25 13:45:13 +0200

    25/08/2018 revisie, verbetering deck, onderverdeling van het spel in fases

--- src/com/pluralsight/itphblackjack/Main.java
+++ src/com/pluralsight/itphblackjack/Main.java
@@ -1,62 +1,18 @@
 package com.pluralsight.itphblackjack;
 
-import java.util.Scanner;
 
     public class Main {
         public static void main(String[] args) {
-           Scanner sc = new Scanner(System.in);
-           Deck deck = new Deck();
-           boolean gevraagd = false;
-           boolean gepast = false;
-           boolean keuze = false;
-
-    System.out.println("Welkom bij BlackJack, wilt u het geschudde deck kaarten zien? J/N?");
-//hile loop die vraagt of de speler het geschudde deck wil zien.
-    while (!keuze) {
-        String schudden = sc.nextLine();
-        schudden = schudden.toUpperCase();
-        deck.schudDeck();
-        if (schudden.equals("J")) {
-
-            deck.bekijkDeck();
-            System.out.println();
-            System.out.println("----------------------------------------");
-            keuze = true;
-        } else if (schudden.equals("N")) {
-            System.out.println("Oké, dan niet!");
-            keuze = true;
-        } else {
-            System.out.println("U heeft geen gepaste keuze gemaakt!");
-            System.out.println("Wilt u het geschudde deck kaarten zien? J/N?");
-        }
-    }
 
 
-    deck.neemKaart();
-
-    //while loop die zorgt voor de toetsen pkq
-    while (!gevraagd && !gepast) {
-        System.out.println("Wat wilt u nu doen: ");
-        System.out.println("Toets p voor pas. ");
-        System.out.println("Toets k voor kaart. ");
-        System.out.println("Toets q om het spel te beïndigen");
-
-        String volgendeMove = sc.nextLine();
-        volgendeMove = volgendeMove.toLowerCase();
-        if (volgendeMove.equals("p")) {
-            gepast = true;
-        } else if (volgendeMove.equals("k")) {
-            deck.neemKaart();
-            gevraagd = true;                    //deze kan eventueel weggehaald worden om meer dan 1 kaart te nemen.
-        } else if (volgendeMove.equals("q")) {
-            System.out.println("Oké, jammer het spel sluit nu af!");
-            System.exit(0);
-        } else {
-            System.out.println("Uw invoer is niet correct, probeer het openieuw!");
-        }
+            
+          Spel spel = new Spel();
+          System.out.println("Welkom bij BlackJack, wilt u het geschudde deck kaarten zien? J/N?");
+          spel.spelFaseEen();
+          spel.spelFaseTwee();
 
 
-    }
+
         System.out.println("Het spel is ten einde gekomen!");
   }
 
