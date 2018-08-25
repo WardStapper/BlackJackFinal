803124249e73c04d1cbfb0110270927fd3d40196
Ward <Ward_Stapper@hotmail.com>
2018-08-25 19:09:03 +0200

    25/08/2018 Versie 2.0

--- src/com/pluralsight/itphblackjack/Spel.java
+++ src/com/pluralsight/itphblackjack/Spel.java
@@ -52,7 +52,7 @@ public class Spel {
         //while loop die zorgt voor de toetsen pkq
 
         while (!spelerInput) {
-            if (spelDeck.totaalscore < 21 || spelDeck.totaalscore !=21) {
+            if (spelDeck.totaalscore < 21 || spelDeck.totaalscore !=21) { //alleen weergave van opties wanneer het spel niet gewonnen of beïndigd is. (spelerinput valt weg zodra er gewonnen of verloren is)
                 System.out.println("Wat wilt u nu doen: ");
                 System.out.println("Toets p voor pas, toets k voor een nieuwe kaart, toets q om het spel te beïndigen.");
             }
