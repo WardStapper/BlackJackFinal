c099cce37451280e4d65f93229917b6e3a77bf1f
Ward <Ward_Stapper@hotmail.com>
2018-08-25 17:12:16 +0200

    25/08/2018 revisie, verbetering deck, onderverdeling van het spel in fases                      - Implementatie reset

--- src/com/pluralsight/itphblackjack/Main.java
+++ src/com/pluralsight/itphblackjack/Main.java
@@ -1,6 +1,6 @@
 package com.pluralsight.itphblackjack;
 
-
+//mainframe waarin het volledige spel afgespeeld word in een regel, na creatie van een spelobject
 public class Main {
         public static void main(String[] args) {
 
